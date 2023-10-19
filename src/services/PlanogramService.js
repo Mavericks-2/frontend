9823b896e35616eae096c9c763c40974f54512b3
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2023-10-18 18:50:36 -0600

    Create Planogram Image Service

--- src/services/PlanogramService.js
+++ src/services/PlanogramService.js
@@ -74,6 +74,43 @@ export async function postPlanogramModel(planogramData) {
   };
 
   reader.onerror = (error) => {
-    console.log("Error8: ", error);
+    console.log("Error: ", error);
+  };
+}
+
+
+export async function postPlanogramImage(planogramData) {
+  const blob = await fetch(planogramData.imagen).then((r) => r.blob());
+  let reader = new FileReader();
+
+  reader.readAsDataURL(blob);
+  reader.onload = () => {
+    let imagenbase4 = reader.result;
+
+    let imagenFinalBase64 = imagenbase4.split(",")[1];
+
+    const bodyPlanogramData = {
+      imagen: imagenFinalBase64,
+    };
+    console.log("bodyPlanogramData: ", bodyPlanogramData);
+
+    fetch(`${API_BASE_URL}/planogram/postPlanogramToCloud`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyPlanogramData),
+    })
+      .then(async (response) => {
+        const data = await response.json();
+        return data.message;
+      })
+      .catch((error) => {
+        console.error("Error uploading image:", error);
+      });
+  };
+
+  reader.onerror = (error) => {
+    console.log("Error: ", error);
   };
 }
