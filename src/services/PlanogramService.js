115c7ad6cd3f9419ee8f964f3f8bba5c21fab27e
Angel Garcia <angel_012912@hotmail.com>
2023-10-19 17:23:41 -0600

    Get url from postPlanogramImage service fixed

--- src/services/PlanogramService.js
+++ src/services/PlanogramService.js
@@ -29,7 +29,7 @@ export async function postPlanogramProducts(planogramData) {
   const bodyPlanogramData = {
     data: {
       coordenadas: planogramData.coordenadas,
-    }
+    },
   };
   const validatePlanogramProducts = await fetch(
     `${FLASK_BASE_URL}/classifyImage`,
@@ -82,38 +82,45 @@ export async function postPlanogramModel(planogramData) {
   };
 }
 
-export async function postPlanogramImage(planogramData) {
-  const blob = await fetch(planogramData.imagen).then((r) => r.blob());
-  let reader = new FileReader();
+const getBase64 = (imageFile) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
 
-  reader.readAsDataURL(blob);
-  reader.onload = () => {
-    let imagenbase4 = reader.result;
-
-    let imagenFinalBase64 = imagenbase4.split(",")[1];
-
-    const bodyPlanogramData = {
-      base_64_image: imagenFinalBase64,
-      type: planogramData.type,
+    reader.onload = () => {
+      let imagenFinalBase64 = reader.result.split(",")[1];
+      resolve(imagenFinalBase64);
     };
 
-    fetch(`${API_BASE_URL}/planogram/postPlanogramToCloud`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(bodyPlanogramData),
-    })
-      .then(async (response) => {
-        const data = await response.json();
-        return data.url;
-      })
-      .catch((error) => {
-        console.error("Error uploading image:", error);
-      });
+    reader.onerror = (error) => {
+      reject(error);
+    };
+
+    reader.readAsDataURL(imageFile);
+  });
+};
+
+export async function postPlanogramImage(planogramData) {
+  const blob = await fetch(planogramData.imagen).then((r) => r.blob());
+  const base64Image = await getBase64(blob);
+  const bodyPlanogramData = {
+    base_64_image: base64Image,
+    type: planogramData.type,
   };
 
-  reader.onerror = (error) => {
-    console.log("Error: ", error);
-  };
+  const url = await fetch(`${API_BASE_URL}/planogram/postPlanogramToCloud`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(bodyPlanogramData),
+  })
+    .then(async (response) => {
+      const data = await response.json();
+      return data.url;
+    })
+    .catch((error) => {
+      console.error("Error uploading image:", error);
+    });
+
+  return url;
 }
