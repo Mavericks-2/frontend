cfbc978b8c8e9d1ae203f5a60865267552c66bd2
ErikaMarlene <eicuch@hotmail.com>
2023-10-31 21:31:07 -0600

    Agregué un PieGraph que funciona correctamente

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,6 +1,9 @@
 import Navbar from "../components/Navbar";
 import { Fragment, useEffect, useState } from "react";
-import { getIntentosPrevAcomodo, getMatrizDiferencias } from "../services/StatusService";
+import {
+  getIntentosPrevAcomodo,
+  getMatrizDiferencias,
+} from "../services/StatusService";
 import ColumnGraph from "../components/ColumnGraph";
 import DashboardsStyle from "../styles/DashboardsStyle.css";
 import PieGraph from "../components/PieGraph";
@@ -9,6 +12,7 @@ import LineGraph from "../components/LineGraph";
 function DashboardPage() {
   const [intentosPrevAcomodoData, setIntentosPrevAcomodoData] = useState([]);
   const [matrizDiferenciaData, setMatrizDiferenciaData] = useState([]);
+
   useEffect(() => {
     getIntentosPrevAcomodo()
       .then((data) => {
@@ -21,28 +25,24 @@ function DashboardPage() {
         );
       });
     getMatrizDiferencias()
-        .then((data) => {
-            setMatrizDiferenciaData(data);
-            // console.log(data);
-        })
-        .catch((error) => {
-            console.error(
-            "Error fetching difference matrix: ",
-            error
-            );
-        });
-  }, [])
+      .then((data) => {
+        setMatrizDiferenciaData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching difference matrix: ", error);
+      });
+  }, []);
 
   function formatFecha(fecha) {
-    const parts = fecha.split('-');
+    const parts = fecha.split("-");
     return `${parts[2]}/${parts[1]}`;
   }
+
   function contarUnosEnMatriz(matrices) {
-    // Convierte la cadena en una matriz bidimensional
     const matricesArray = JSON.parse(`[${matrices}]`);
-    
+
     let contadorUnos = 0;
-    
+
     matricesArray.forEach((matriz) => {
       matriz.forEach((fila) => {
         fila.forEach((elemento) => {
@@ -52,47 +52,65 @@ function DashboardPage() {
         });
       });
     });
-    
+
     return contadorUnos;
   }
+
+  function contarProductos(matricesProductosF) {
+    const matricesProductosFArray = JSON.parse(`[${matricesProductosF}]`);
+    const productoCount = {};
+    matricesProductosFArray.forEach((matriz) => {
+      if (Array.isArray(matriz)) {
+        matriz.forEach((fila) => {
+          if (Array.isArray(fila)) {
+            fila.forEach((producto) => {
+              if (productoCount[producto]) {
+                productoCount[producto]++;
+              } else {
+                productoCount[producto] = 1;
+              }
+            });
+          }
+        });
+      }
+    });
+    return productoCount;
+  }
+  
+  const productosContados = contarProductos(
+    matrizDiferenciaData.map((item) => item.matricesProductosF)
+  );
+
+  const productosContadosArray = Object.keys(productosContados).map((producto) => ({
+    producto: producto,
+    count: productosContados[producto],
+  }));
   
 
   return (
     <>
       <Navbar />
-      {/* <div className="container">
-            <div className="row">
-                <div className="col-12">
-                <h1>Dashboard</h1>
-                <ul>
-                {intentosPrevAcomodoData.length > 0 ? (
-                    intentosPrevAcomodoData.map((intentos, index) => (
-                    <li key={index}>
-                        <p>Id Acomodador: {intentos.id_acomodador}</p>
-                        <p>Nombre Acomodador: {intentos.statusAcomodador.nombre}</p>
-                        <p>Fecha: {intentos.fecha}</p>
-                        <p>Conteo: {intentos.conteo}</p>
-                    </li>
-                    ))
-                ) : (
-                    <p>Cargando datos...</p>
-                )}
-                </ul>
-                <ul>
-                    {matrizDiferenciaData.length > 0 ? (
-                        matrizDiferenciaData.map((matriz, index) => (
-                            <li key={index}>
-                                <p>Matriz: {matriz.matricesDiferencias}</p>
-                                <p>Fecha: {matriz.fecha}</p>
-                                </li>
-                        ))
-                    ) : (
-                        <p>Cargando datos...</p>
-                    )}
-                </ul>
-                </div>
-            </div>
-        </div> */}
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            {/* <ul>
+              {matrizDiferenciaData.length > 0 ? (
+                matrizDiferenciaData.map((matriz, index) => (
+                  <li key={index}>
+                    <p>Matriz: {matriz.matricesDiferencias}</p>
+                    <p>
+                      Matriz productos fallidos: {matriz.matricesProductosF}
+                    </p>
+                    <p>Fecha: {matriz.fecha}</p>
+                  </li>
+                ))
+              ) : (
+                <p>Cargando datos...</p>
+              )}
+            </ul> */}
+          </div>
+        </div>
+      </div>
       <Fragment>
         <div className="dashboards-main-container">
           <div className="header">
@@ -104,10 +122,10 @@ function DashboardPage() {
                 Número de intentos incorrectos
               </div>
               <ColumnGraph
-                data={intentosPrevAcomodoData.map(item => ({
-                    ...item,
-                    fecha: formatFecha(item.fecha)
-                  }))}
+                data={intentosPrevAcomodoData.map((item) => ({
+                  ...item,
+                  fecha: formatFecha(item.fecha),
+                }))}
                 xField={"fecha"}
                 yField={"conteo"}
               />
@@ -117,15 +135,21 @@ function DashboardPage() {
                 Número de productos fallidos
               </div>
               <LineGraph
-              data = {matrizDiferenciaData.map(item => ({
-                ...item,
-                unos: contarUnosEnMatriz(item.matricesDiferencias),
-                fecha: formatFecha(item.fecha)
-              }))}
-              xField={"fecha"}          
-              yField={"unos"}
-              value={"unos"}
-               />
+                data={matrizDiferenciaData.map((item) => ({
+                  ...item,
+                  unos: contarUnosEnMatriz(item.matricesDiferencias),
+                  fecha: formatFecha(item.fecha),
+                }))}
+                xField={"fecha"}
+                yField={"unos"}
+              />
+            </div>
+            <div className="dashboards-item">
+              <div className="dashboard-item-title">
+                Productos fallidos
+              </div>
+              <PieGraph data={productosContadosArray} xField="producto" yField="count" />
+
             </div>
           </div>
         </div>
