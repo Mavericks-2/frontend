e58655bc9d5495b869dc0b78ba847b8064c82ad8
Angel Garcia <angel_012912@hotmail.com>
2023-12-02 21:12:11 -0600

    Fotos de equipo actualizadas

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -5,6 +5,9 @@ import AngelPP from "../assets/angel-pp.jpeg";
 import EliPP from "../assets/eli-pp.jpeg";
 import GabyPP from "../assets/gaby-pp.jpeg";
 import ZayPP from "../assets/zay-pp.jpeg";
+import AnaPP from "../assets/ana-pp.jpeg";
+import ChemaPP from "../assets/chema-pp.jpeg";
+import ErikaPP from "../assets/erika-pp.jpeg";
 
 function TeamSection() {
   return (
@@ -14,7 +17,7 @@ function TeamSection() {
           <img src={PabloPP} className="imageEquipo" />
           <div class="box-content">
             <h3 class="title">Pablo Gonzalez de la Parra</h3>
-            <span class="post">Web Designer</span>
+            <span class="post">ING. TECNOLOGÍAS COMPUTACIONALES</span>
           </div>
         </div>
       </div>
@@ -23,7 +26,16 @@ function TeamSection() {
           <img src={AngelPP} className="imageEquipo" />
           <div class="box-content">
             <h3 class="title">Jose Angel Garcia Gomez</h3>
-            <span class="post">Web Developer</span>
+            <span class="post">ING. TECNOLOGÍAS COMPUTACIONALES</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <div class="box">
+          <img src={ErikaPP} className="imageEquipo" />
+          <div class="box-content">
+            <h3 class="title">Erika Marlene García Sanchez</h3>
+            <span class="post">ING. TECNOLOGÍAS COMPUTACIONALES</span>
           </div>
         </div>
       </div>
@@ -32,7 +44,7 @@ function TeamSection() {
           <img src={EliPP} className="imageEquipo" />
           <div class="box-content">
             <h3 class="title">Elisa Sanchez Bahnsen</h3>
-            <span class="post">Web Developer</span>
+            <span class="post">ING. CIENCIA DE DATOS Y MATEMÁTICAS</span>
           </div>
         </div>
       </div>
@@ -40,8 +52,8 @@ function TeamSection() {
         <div class="box">
           <img src={GabyPP} className="imageEquipo" />
           <div class="box-content">
-            <h3 class="title">Gabriel Cortes Olvera</h3>
-            <span class="post">Web Developer</span>
+            <h3 class="title">Gabriela Cortes Olvera</h3>
+            <span class="post">ING. CIENCIA DE DATOS Y MATEMÁTICAS</span>
           </div>
         </div>
       </div>
@@ -50,7 +62,25 @@ function TeamSection() {
           <img src={ZayPP} className="imageEquipo" />
           <div class="box-content">
             <h3 class="title">Zaide Islas Montiel</h3>
-            <span class="post">Web Developer</span>
+            <span class="post">ING. CIENCIA DE DATOS Y MATEMÁTICAS</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <div class="box">
+          <img src={AnaPP} className="imageEquipo" />
+          <div class="box-content">
+            <h3 class="title">Ana Martínez Barbosa</h3>
+            <span class="post">ING. CIENCIA DE DATOS Y MATEMÁTICAS</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6">
+        <div class="box">
+          <img src={ChemaPP} className="imageEquipo" />
+          <div class="box-content">
+            <h3 class="title">José María Ibarra Pérez</h3>
+            <span class="post">ING. CIENCIA DE DATOS Y MATEMÁTICAS</span>
           </div>
         </div>
       </div>
